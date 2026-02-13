3363a764340702d48c907417f08e741148906f27
Guillem Castells <guillem.castells@gmail.com>
2026-02-13 15:30:00 +0000

    Add Windows build helper scripts

---
+++ scripts/run_test.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "%~dp0\.."
+call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat" >nul 2>&1
+set TORCH_CUDA_ARCH_LIST=7.5
+python tests\test_forward.py
